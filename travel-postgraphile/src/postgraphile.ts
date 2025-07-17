ea8b50c8bf5004e66a2c16767768f34ad0c6dcbf
neeshan-ibn-ismath <neeshanbinismath@gmail.com>
2025-07-17 13:29:25 +0530

    Refactor postgraphile configuration for improved readability and consistency

--- travel-postgraphile/src/postgraphile.ts
+++ travel-postgraphile/src/postgraphile.ts
@@ -1,21 +1,25 @@
-import { postgraphile } from 'postgraphile'
-import ConnectionFilterPlugin from "postgraphile-plugin-connection-filter"
+import { postgraphile } from 'postgraphile';
+import ConnectionFilterPlugin from 'postgraphile-plugin-connection-filter';
 
-const { DATABASE, PG_USER, PASSWORD, HOST} = process.env
+const { DATABASE, PG_USER, PASSWORD, HOST } = process.env;
 
 export default postgraphile(
-    {
-        database: DATABASE,
-        user: PG_USER,
-        password: PASSWORD,
-        host: HOST,
-        port: 5432,
-    },
-    'public',
-    {
-        watchPg: true,
-        graphiql: true,
-        enhanceGraphiql: true,
-        appendPlugins: [ConnectionFilterPlugin],
-    }
-)
+  {
+    database: DATABASE,
+    user: PG_USER,
+    password: PASSWORD,
+    host: HOST,
+    port: 5432,
+  },
+  'public',
+  {
+    watchPg: true,
+    graphiql: true,
+    enhanceGraphiql: true,
+    appendPlugins: [ConnectionFilterPlugin],
+
+    // detailed error messages
+    showErrorStack: 'json',
+    extendedErrors: ['hint', 'detail', 'errcode', 'message', 'stack'],
+  }
+);
